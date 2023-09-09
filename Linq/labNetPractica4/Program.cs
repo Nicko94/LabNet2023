537d7d0c9b470f0bb86ab1675599f6a6b3cc84ba
Nicko94 <nicolaslicastro@gmail.com>
2023-09-09 00:44:11 -0300

    Add files via upload

--- Linq/labNetPractica4/Program.cs
+++ Linq/labNetPractica4/Program.cs
@@ -1,11 +1,4 @@
-﻿using LabNetPractica4.Logic;
-using LabNetPractica4.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LabNetPractica4.UI;
+﻿using LabNetPractica4.UI;
 
 namespace labNetPractica4.UI
 {
@@ -19,24 +12,6 @@ namespace labNetPractica4.UI
             {
                 Menu.ExecuteMenu();
             }
-            /*
-            1. Query para devolver objeto customer ------------
-            2. Query para devolver todos los productos sin stock ---------------
-            3. Query para devolver todos los productos que tienen stock y que cuestan más de 3 por unidad ----------
-            4. Query para devolver todos los customers de la Región WA
-            5. Query para devolver el primer elemento o nulo de una lista de productos donde el ID de producto sea igual a 789
-            6. Query para devolver los nombre de los Customers. Mostrarlos en Mayuscula y en minuscula.
-            7. Query para devolver Join entre Customers y Orders donde los customers sean de la Región WA y la fecha de orden sea mayor a 1/1/1997.
-
-            Los siguientes ejercicios no son obligatorios, pero es deseable que estén resueltos:
-
-            8. Query para devolver los primeros 3 Customers de la  Región WA
-            9. Query para devolver lista de productos ordenados por nombre
-            10. Query para devolver lista de productos ordenados por "unit in stock" de mayor a menor.
-            11. Query para devolver las distintas categorías asociadas a los productos
-            12. Query para devolver el primer elemento de una lista de productos
-            13. Query para devolver los customer con la cantidad de ordenes asociadas
-            */
         }
     }
 }
